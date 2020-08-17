668c25f78e1fe9e0b45e8df1432ba8e1e3154e59
sebas_z <zanabria.s@pucp.pe>
2020-08-16 23:56:06 -0500

    Relation model migrations added

---
+++ db/migrate/20200814175013_create_visits.rb
@@ -0,0 +1,10 @@
+class CreateVisits < ActiveRecord::Migration[6.0]
+  def change
+    create_table :visits do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :property, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

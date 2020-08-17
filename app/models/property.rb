07e51fba2841f453e5c560232c30388026afe260
sebas_z <zanabria.s@pucp.pe>
2020-08-16 23:59:59 -0500

    Relation model associations added

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,10 @@
 
 class Property < ApplicationRecord
   belongs_to :landlord, class_name: 'User'
+  has_many :favorites
+  has_many :lovers, through: :favorites, source: :user
+  has_many :contacts
+  has_many :applicants, through: :contacts, source: :user
+  has_many :visits
+  has_many :visitors, through: :visits, source: :user
 end

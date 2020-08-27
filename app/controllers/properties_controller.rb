ae8179ffd035715aaaefed19247dd5cdfae0a8e4
Freddy Munive <munivef@yahoo.com>
2020-08-27 16:53:21 -0500

    adding queries to get properties part 1

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,21 @@
 class PropertiesController < ApplicationController
   def index
-    @properties = Property.all
+    @properties = []
+    operation = params[:operation]
+    property = params[:property]
+    place = params[:place]
+        # @properties = Property.where("property_type = 'apartment' AND operation_type = 'rent' AND district LIKE 'L%'")
+    if operation or property
+      if !operation == "" and !property == ""
+        @properties = Property.where(property_type: property , operation_type: operation )
+      else
+        @properties = Property.where(operation_type: operation) unless operation == ""
+        @properties = Property.where(property_type: property) unless property == ""
+        @properties = Property.all if operation == "" and property == ""
+      end
+    else
+      @properties = Property.all
+    end
     render json:@properties
   end
 
@@ -13,5 +28,10 @@ class PropertiesController < ApplicationController
     rescue => exception
       render json: {error: "property not found"}
     end
+  end
+
+  def lastest
+    @properties = Property.limit(3).order(created_at: :desc)
+    render json:@properties
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # get 'properties/index'
   # get 'properties/show'
+  get "/lastest", to: "properties#lastest"
   resources :properties, only: [:index, :show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

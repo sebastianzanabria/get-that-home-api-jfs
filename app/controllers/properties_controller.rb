061c8dbf7db7002751533ebeaa687d798febd51a
sebas_z <zanabria.s@pucp.pe>
2020-08-28 13:02:15 -0500

    Properties controller refactoring notes added

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,37 +1,48 @@
+# frozen_string_literal: true
+
 class PropertiesController < ApplicationController
+  skip_before_action :require_login, only: %i[index show]
+
   def index
+    # Se debe filtrar por el nombre del campo, ej operation_type en lugar de operation (Conversar con Front)
+    # Se debe filtrar si es que el Property tiene "is_available" igual a true
+    # Se debe incluir los filtros para los demas campos (bedrooms, bathrooms, apartment amenities)
+    # Se debe recoger los datos con un property_params. OJO con campos tipo array
+    # se debe incluir el local storage para imagenes
     @properties = []
     operation = params[:operation]
     property = params[:property]
     place = params[:place]
-        # @properties = Property.where("property_type = 'apartment' AND operation_type = 'rent' AND district LIKE 'L%'")
-    if operation or property
-      if !operation == "" and !property == ""
-        @properties = Property.where(property_type: property , operation_type: operation )
+    # @properties = Property.where("property_type = 'apartment' AND operation_type = 'rent' AND district LIKE 'L%'")
+    if operation || property
+      if (!operation == '') && (!property == '')
+        @properties = Property.where(property_type: property, operation_type: operation)
       else
-        @properties = Property.where(operation_type: operation) unless operation == ""
-        @properties = Property.where(property_type: property) unless property == ""
-        @properties = Property.all if operation == "" and property == ""
+        unless operation == ''
+          @properties = Property.where(operation_type: operation)
+        end
+        unless property == ''
+          @properties = Property.where(property_type: property)
+        end
+        @properties = Property.all if (operation == '') && (property == '')
       end
     else
       @properties = Property.all
     end
-    render json:@properties
+    render json: @properties, status: :ok
   end
 
   def show
-    begin
-      @property = Property.find(params[:id])
-      if @property
-        render json:@property
-      end
-    rescue => exception
-      render json: {error: "property not found"}
-    end
+    @property = Property.find(params[:id])
+    render json: @property, status: :ok if @property
+  rescue StandardError => e
+    render json: { error: 'property not found' }, status: :not_found
   end
+
+  # Dentro del metodo "create" este solo puede ser usado por "landlords"
 
   def lastest
     @properties = Property.limit(3).order(created_at: :desc)
-    render json:@properties
+    render json: @properties, status: :ok
   end
 end
